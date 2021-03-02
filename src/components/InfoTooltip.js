6ff64500392738bcf441a090dfef179c455e116b
Alexey Krasnov <neky@list.ru>
2021-03-02 10:08:27 +0500

    fix

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import sucess from '../images/sucess.svg';
+import fail from '../images/fail.svg';
 
 function InfoTooltip(props) {
 
@@ -6,8 +8,8 @@ function InfoTooltip(props) {
         <section className={props.isOpen ? 'popup popup_opened' : 'popup'} onClick={props.onPopupOverlayClose}>
             <div className="popup__container">
                 <div className="popup__area popup__area_tooltip">
-                    <img className="popup__tooltip-icon" src={props.src} alt="иконка результата"/>
-                    <h2 className={`popup__tooltip-text`}>{props.title}</h2>
+                    <img className="popup__tooltip-icon" src={props.isTooltipStatus ? sucess : fail} alt="иконка результата"/>
+                    <h2 className={`popup__tooltip-text`}>{props.isTooltipStatus ? "Вы успешно зарегистрировались!" : "Что-то пошло не так! Попробуйте ещё раз."}</h2>
                     <button className="popup__close" type="button" onClick={props.onClose}/>
                 </div>
             </div>
